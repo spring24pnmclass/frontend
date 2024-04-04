09c98aa418109d6de7bd97bf8807b12fea7710c6
josh-hsiehh <joshua.hsieh@sjsu.edu>
2024-04-03 23:42:52 -0700

    website

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+import instagramLogo from './instagramLogo.png'
+import map from './map.png'
+import pnmclasspic from './pnmclasspic.png'
+import signUpLogo from './signUpLogo.png'
+import transparentDiscord from './transparentDiscord.png'
+
+
+export const assets = {
+   instagramLogo,
+   map,
+   pnmclasspic,
+   signUpLogo,
+   transparentDiscord 
+}
---
+++ src/components/pages/Order.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Order = () => {
+  return <h1>Order</h1>;
+};
