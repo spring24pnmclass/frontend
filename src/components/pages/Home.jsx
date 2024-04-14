553b09822a8e9dd2ba57b453793cb3b0dccb6664
Mahek K <145400846+mahekkothari@users.noreply.github.com>
2024-04-14 01:33:08 -0700

    home and nav bar changes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import BannerComponent from "./BannerComponent"; // Import the BannerComponent
+import "../Home.css";
 
 export const Home = () => {
   return (
-    <div className="home-container" style={{ backgroundColor: "#f0f0f0" }}>
-      {/* Adding a background color */}
-      <BannerComponent /> {/* Render the BannerComponent here */}
-      <h1>Welcome to our website!</h1>
-      {/* Adding placeholder text */}
-      <p>This is just a placeholder text to test if the home component works.</p>
+    <div>
+      <div className="banner-container">
+        <BannerComponent />
+      </div>
+      
+      <div className="text-container">
+        <br>
+        </br>
+        <p>How you can use our product:</p>
+      </div>
+
+      <div className="columns-container">
+        <div className="column">
+          <h1>1</h1>
+          <p>If you every find yourself hungry, yet in an awkward area on campus just open this webiste. Then, figure out what you want to eat and then go to the order page</p>
+        </div>
+        <div className="column">
+          <h1>2</h1>
+          <p>After choosing your specifc food just click our easy order bottom!</p>
+        </div>
+        <div className="column">
+          <h1>3</h1>
+          <p>After you order, your food will be delivered shortly through the to-go bot.</p>
+        </div>
+      </div>
     </div>
   );
 };
