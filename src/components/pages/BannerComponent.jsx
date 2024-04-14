008ebfbf7120b0b51c62b832eb400ae89bb408de
Mahek K <145400846+mahekkothari@users.noreply.github.com>
2024-04-13 22:18:26 -0700

    o

--- src/components/pages/BannerComponent.jsx
+++ src/components/pages/BannerComponent.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../banner.css";
 
 const BannerComponent = () => {
   return (
     <div className="banner">
       <div className="banner-content">
         <div className="banner-text">
-          <h2>Revolutionize food delivery, one autonomous bot at a time.</h2>
+        <h2>Revolutionize food delivery, one autonomous bot at a time.</h2>
           <p>We fundamentally believe that college students should have acess to food, and through our autonomous food delivery robot we will bring food to students! </p>
           <div class="box_3JAMv loadMore_bVOpc">
             <div class="scroller_dt8Fd"></div>
