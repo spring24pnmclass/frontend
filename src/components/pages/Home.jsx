90e8328bdbe884e8f65b61bc69c5092367b3d849
Mahek K <145400846+mahekkothari@users.noreply.github.com>
2024-04-13 20:31:23 -0700

    o

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import "../Home.css"; 
+import BannerComponent from "./BannerComponent";
 
-export const Home = () => {
-  return <h1>Home</h1>;
+const Home = () => {
+  return (
+    <div className="home-container" style={{ backgroundColor: "#f0f0f0" }}>
+      {/* Adding a background color */}
+      <BannerComponent />
+      <h1>Welcome to our website!</h1>
+      {/* Adding placeholder text */}
+      <p>This is just a placeholder text to test if the home component works.</p>
+    </div>
+  );
 };
+
+export default Home;
--- src/components/pages/Order.jsx
+++ src/components/pages/Order.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export const Order = () => {
-  return <h1>Order</h1>;
+  return (
+    <div id = "root">
+     <div style={{backgroundColor: '#F5EBE0'}}>
+
+  <div className = "description-box">
+    <h2>At the press of a button, the autonomous robot will deliver food to your location!</h2>
+  </div>
+
+  <div className = "button">
+    <h2>Order</h2>
+  </div>
+
+  <div className = "placeholder-box">
+    <h2>           </h2>
+    <br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br>
+  </div>
+  </div> 
+  </div>
+  );
 };
