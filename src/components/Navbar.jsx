a185fc2f87077a3cc6b6e265f1e65758c6b79ac6
Mahek K <145400846+mahekkothari@users.noreply.github.com>
2024-04-14 21:24:15 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav>
       <Link to="/" className="title">
-        To-Go Bot
+        TogoBot
       </Link>
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
